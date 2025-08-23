053e195a3cf918def9a106e96182852581f41770
pankajswamivaishnav <pankajvaishnav128@gmail.com>
2025-08-23 18:51:48 +0530

    update in conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 mongoose
-  .connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGODB_URL)
   .then(() => {
     console.log("Database connected successfully");
   })
